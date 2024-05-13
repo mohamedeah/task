63c1382809ee3bd5c513cde38b03b4c7014420c8
unknown <m_hafez1989@hotmail.com>
2024-05-13 20:01:48 +0200

    add python1

---
+++ paython1.py
@@ -0,0 +1,55 @@
+# x = True
+# y = 100
+# z = 8.59
+# s = ("mohamed")
+# print(float(y))
+#we can't convert the str to int
+# list_of_numbers = (1 , 2 , 3 , 4 , 5)
+# list_of_numbers = [10 , 11 , 12 , 13 , 14 , 15]
+# list_of_numbers = [1 , 2 , 3 , 4 , 5]
+#dict = {"name"  ; "mohamed", "age"  ;  32, "salary" ;  15000}
+#we can't use semi colon in python
+# Python is interpreted or compiled
+# What is the differences between low level & high level
+# == to show the equality btw values / = used to assign the value
+# != to show the inequality btw values
+# the priority of operator precedence in python if we talk about codes execut is from up to down also the input code has priority than print order 
+# x = 10 
+# print(x+15)
+# print(x/5)
+# print(x*10)
+# print(x%3)
+#print(11,10,9,8,7,6,5,4)
+#print(2**3)
+#print(11/3)
+# c = 11/3 
+# print(int(c))
+# print(int(11/3))
+# x = 10 
+# if  x>15:
+#     print(True)
+# else: print(x,"is smaller than 15")
+# In which cases we will use all
+# What is the differences between all , and
+# What is the differences between any , or
+# If we need all the conditions to be true we will use ….
+# What is the differences between if , elif  if is consider the main condition elif allow you to check additional coditions if the previous coditions are false
+# What is the differences between elif else elif is an additional condition want to be check - else the final condition if the all previous conditions are false 
+# Can we use more than one elif ( yes)
+# Can we use more than one else (no)
+# write s simple ternary operator
+# in elif , python will check all the conditions no matter what ? no the code will stop with the first true condition 
+#in elif we use else for
+numbers = [2,4,6,8,10]
+# if 4 in numbers:
+#     print(True)
+# else:print(False)
+# if 4 and 6 in numbers:
+#     print(True)
+# else:print(False)
+# if 3 or 6 in numbers:
+#     print(True)
+# else:print(False)
+if 2 and 4 and 5 in numbers:
+    print(True)
+else:print(False)
